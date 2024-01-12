b6503ef20aee3345eed7d533b722f48061e63ad8
Alexandre Duval <alexsantosduval@outlook.com>
2024-01-12 01:36:22 +0000

    minor changes

--- guess_number_game.py
+++ guess_number_game.py
@@ -6,27 +6,27 @@ Min_value = 1
 
 
 # function generates number between minimum and maximum value.
-def generate_number(min: int, max: int) -> int:
+def generate_number(min: int, max: int):
     return random.randint(min, max)
 
 
 # function asks input from user and checks if its higher, lower or equal to the generated number.
-def guess_number(generate_number: int, max: int, min: int) -> int:
+def guess_number(number: int, max: int, min: int):
     while True:
-        guess = input("Guess a number between 1 and 100: ")
+        guess = input("Guess a number between 1 and 100 or press 0 to exit: ")
         try:
             guess = int(guess)
             if guess <= max and guess >= min:
-                if guess < generate_number + 5 and guess > generate_number - 5:
-                    print("You're close!")
-                elif guess < generate_number:
-                    print("Too low.")
-                elif guess > generate_number:
-                    print("Too high.")
-                elif guess == generate_number:
-                    print("Congrats you guessed right!")
-                    return False
-            # if the user types the number 0 it will allow them quit the game.
+                if (guess < number + 5 and guess > number - 5) and guess != number:
+                    print("You're close!\n")
+                elif guess < number:
+                    print("Too low.\n")
+                elif guess > number:
+                    print("Too high.\n")
+                elif guess == number:
+                    print("Congrats you guessed right!\n")
+                    break
+            # if the user types the number 0 it will give the user the option to generate a new number or quit.
             elif guess == 0:
                 return False
             else:
@@ -36,9 +36,9 @@ def guess_number(generate_number: int, max: int, min: int) -> int:
 
 
 # function that allows user to choose to replay the game or not.
-def replay() -> bool:
+def replay():
     while True:
-        retry = input("Would you like to play again? (Y/N) ")
+        retry = input("Play again? (Y/N) ".lower())
         if retry == "y":
             return True
         if retry == "n":
@@ -54,7 +54,7 @@ def game_loop():
         guess_number(generate_number(Min_value, Max_value), Max_value, Min_value)
         if replay() == False:
             break
-    print("\nThanks for playing!\n")
+    print("Thanks for playing!\n")
 
 
 game_loop()
